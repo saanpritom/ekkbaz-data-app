b5be6bb1f0d4615c8330cc5f5c80f4de90cb659e
saanpritom <pritom.saan@gmail.com>
2022-11-23 23:51:03 +0600

    production port is changed.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ set -e
 python app/manage.py makemigrations
 python app/manage.py migrate
 python app/manage.py collectstatic --no-input
-sh -c "cd /service/app/ && gunicorn --workers 5 --timeout 60 --bind 0.0.0.0:8000 conf.wsgi:application --daemon && nginx -g 'daemon off;'"
+sh -c "cd /service/app/ && gunicorn --workers 5 --timeout 60 --bind 0.0.0.0:7000 conf.wsgi:application --daemon && nginx -g 'daemon off;'"
 exec "$@"
